5c144bae2463d7e327b96b080192c64a2c6788af
Bemi Faison <bemson@gmail.com>
2011-07-24 01:05:11 -0700

    Shim for Array method .indexOf()
    Succumbing to IE >:-(

--- src/flow.pkg.core.js
+++ src/flow.pkg.core.js
@@ -120,6 +120,44 @@ Flow Package: core
     }),
     activeFlows = []; // collection of active flows
 
+  // Relies on shims from Flow library: .every(), .filter(), .forEach(), .map()
+  // shim Array.prototype.indexOf()
+  if (!activeFlows.indexOf) {
+    Array.prototype.indexOf = function(searchElement /*, fromIndex */) {
+      "use strict";
+
+      if (this === void 0 || this === null)
+        throw new TypeError();
+
+      var t = Object(this);
+      var len = t.length >>> 0;
+      if (len === 0)
+        return -1;
+
+      var n = 0;
+      if (arguments.length > 0) {
+        n = Number(arguments[1]);
+        if (n !== n) // shortcut for verifying if it's NaN
+          n = 0;
+        else if (n !== 0 && n !== (Infinity) && n !== -(Infinity))
+          n = (n > 0 || -1) * Math.floor(Math.abs(n));
+      }
+
+      if (n >= len)
+        return -1;
+
+      var k = n >= 0
+            ? n
+            : Math.max(len - Math.abs(n), 0);
+
+      for (; k < len; k++) {
+        if (k in t && t[k] === searchElement)
+          return k;
+      }
+      return -1;
+    };
+  }
+
   // define traversal event names
   core.events = 'on|in|out|over'.split('|');
 
