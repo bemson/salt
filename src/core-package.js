68625bf9d17f02c8dc03609846b5b953993cefad
Bemi Faison <bemson@gmail.com>
2011-10-21 10:44:30 -0700

    Optimized Array.indexOf() and Array.every() shims

--- src/core-package.js
+++ src/core-package.js
@@ -123,57 +123,25 @@
         flags.omit = 1;
       }
     }),
-    activeFlows = []; // collection of active flows
+    activeFlows = [], // collection of active flows
+    arrayPrototype = Array.prototype; // alias for minification purporses
 
-  if (!activeFlows.every) {
-    Array.prototype.every = function(fun /*, thisp */) {
-      "use strict";
-
-      if (this === void 0 || this === null)
-        throw new TypeError();
-
-      var t = Object(this);
-      var len = t.length >>> 0;
-      if (typeof fun !== "function")
-        throw new TypeError();
-
-      var thisp = arguments[1];
-      for (var i = 0; i < len; i++) {
-        if (!fun.call(thisp, t[i], i, t))
+  if (!arrayPrototype.every) {
+    arrayPrototype.every = function(fnc, scope) {
+      for (var i = 0, j = this.length; i < j; i++) {
+        if (!fnc.call(scope, this[i], i, this)) {
           return false;
+        }
       }
-
       return true;
     };
   }
 
-  if (!activeFlows.indexOf) {
-    Array.prototype.indexOf = function (searchElement /*, fromIndex */ ) {
-      "use strict";
-      if (this === void 0 || this === null) {
-        throw new TypeError();
-      }
-      var t = Object(this);
-      var len = t.length >>> 0;
-      if (len === 0) {
-        return -1;
-      }
-      var n = 0;
-      if (arguments.length > 0) {
-        n = Number(arguments[1]);
-        if (n !== n) { // shortcut for verifying if it's NaN
-          n = 0;
-        } else if (n !== 0 && n !== Infinity && n !== -Infinity) {
-          n = (n > 0 || -1) * Math.floor(Math.abs(n));
-        }
-      }
-      if (n >= len) {
-        return -1;
-      }
-      var k = n >= 0 ? n : Math.max(len - Math.abs(n), 0);
-      for (; k < len; k++) {
-        if (k in t && t[k] === searchElement) {
-          return k;
+  if (!arrayPrototype.indexOf) {
+    arrayPrototype.indexOf = function (searchElement, fromIndex) {
+      for (var i = (fromIndex | 0), j = this.length; i < j; i++) {
+        if (this[i] === searchElement) {
+          return i;
         }
       }
       return -1;
