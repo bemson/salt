efab9eb5b0ce20491da61467e3f68775897c1d2e
Bemi Faison <bemson@gmail.com>
2011-10-21 09:30:33 -0700

    Fixed proxy.go() waypoint replacement. Added Array.indexOf() shim.

--- src/core-package.js
+++ src/core-package.js
@@ -147,6 +147,39 @@
     };
   }
 
+  if (!activeFlows.indexOf) {
+    Array.prototype.indexOf = function (searchElement /*, fromIndex */ ) {
+      "use strict";
+      if (this === void 0 || this === null) {
+        throw new TypeError();
+      }
+      var t = Object(this);
+      var len = t.length >>> 0;
+      if (len === 0) {
+        return -1;
+      }
+      var n = 0;
+      if (arguments.length > 0) {
+        n = Number(arguments[1]);
+        if (n !== n) { // shortcut for verifying if it's NaN
+          n = 0;
+        } else if (n !== 0 && n !== Infinity && n !== -Infinity) {
+          n = (n > 0 || -1) * Math.floor(Math.abs(n));
+        }
+      }
+      if (n >= len) {
+        return -1;
+      }
+      var k = n >= 0 ? n : Math.max(len - Math.abs(n), 0);
+      for (; k < len; k++) {
+        if (k in t && t[k] === searchElement) {
+          return k;
+        }
+      }
+      return -1;
+    }
+  }
+
   // returns true when the argument is a valid variable name
   function isVariableNameValid(name) {
     return name != null && /\w/.test(name);
@@ -840,7 +873,7 @@
             tgts = waypoints.concat(tgts);
           } else { // otherwise, when in the _on[0] phase...
             // replace the current target with the waypoints
-            tgts.splice.apply(tgts,[0, 1,].concat(waypoints));
+            tgts.splice.apply(tgts,[0, 1].concat(waypoints));
           }
         } else { // otherwise, when there are targets...
           // set route to waypoints
