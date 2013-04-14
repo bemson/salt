3076ae8d0c5db896d03c37b7701ccd0b3ec8c0db
Bemi Faison <bemson@gmail.com>
2013-04-14 03:00:00 -0700

    Re #20 & #37 More `.vars` unit tests

--- test/api.flow.vars.js
+++ test/api.flow.vars.js
@@ -2,63 +2,127 @@ describe( 'Flow#vars', function () {
 
   var flow;
 
-  it( 'should be an object member, only present while navigating', function () {
+  it( 'should be a navigation-only member', function () {
     var
-      varsRef,
-      inSpy = sinon.spy(function () {
+      spy = sinon.spy(),
+      varsRef
+    ;
+    flow = new Flow({
+      _in: function () {
         this.should.haveOwnProperty('vars');
         this.should.be.an('object');
         varsRef = this.vars;
-      }),
-      onSpy = sinon.spy(function () {
+      },
+      _on: function () {
         this.vars.should.equal(varsRef);
-      })
-    ;
-    flow = new Flow({
-      _in: inSpy,
-      _on: onSpy
+        spy();
+      }
     });
     flow.go(1);
-    inSpy.should.have.been.calledOnce;
-    onSpy.should.have.been.calledOnce;
+    spy.should.have.been.calledOnce;
     flow.should.not.haveOwnProperty('vars');
   });
 
-  it( 'should ignore being set anything other than an object', function () {
+  it( 'should be an object', function () {
+    var spy = sinon.spy(function () {
+      this.vars.should.be.an('object');
+    });
+    flow = new Flow(spy);
+    flow.go(1);
+    spy.should.have.been.calledOnce;
+  });
+
+  it( 'should allow being set to an object', function () {
     var
-      inSpy = sinon.spy(function () {
-        this.vars.should.be.an('object');
-        this.vars = 'scalar value';
-      }),
-      onSpy = sinon.spy(function () {
-        this.vars.should.be.an('object');
-      }),
+      replacementObj = {},
+      spy = sinon.spy()
+    ;
     flow = new Flow({
-      _in: inSpy,
-      _on: onSpy
+      _in: function () {
+        this.vars = replacementObj;
+      },
+      _on: function () {
+        this.vars.should.equal(replacementObj);
+        spy();
+      }
     });
     flow.go(1);
-    inSpy.should.have.been.calledOnce;
-    onSpy.should.have.been.calledOnce;
+    spy.should.have.been.calledOnce;
+  });
+
+  it( 'should restore itself when set to scalar values', function () {
+    var
+      value = {},
+      spy = sinon.spy()
+    ;
+    flow = new Flow({
+      _in: function () {
+        this.vars.foo = value;
+        this.vars = 'scalar value';
+      },
+      _on: function () {
+        this.vars.should.be.an('object');
+        this.vars.should.haveOwnProperty('foo');
+        this.vars.foo.should.equal(value);
+        spy();
+      }
+    });
+    flow.go(1);
+    spy.should.have.been.calledOnce;
   });
 
   it( 'should discard members after ending on the `null` state', function () {
-    var
-      onSpy = sinon.spy(function () {
-        this.vars.foo = 10;
-      }),
-      aSpy = sinon.spy(function () {
-        this.vars.should.be.empty;
-      })
-    ;
+    var spy = sinon.spy();
     flow = new Flow({
-      _on: onSpy,
-      a: aSpy
+      _on: function () {
+        this.vars.foo = 'bar';
+      },
+      check: function () {
+        this.vars.should.be.empty;
+        spy();
+      }
     });
     flow.go(1, 0);
-    flow.go('//a/');
-    onSpy.should.have.been.calledOnce;
-    aSpy.should.have.been.calledOnce;
+    flow.status().state.should.equal('_null');
+    flow.go('//check');
+    spy.should.have.been.calledOnce;
+  });
+
+  it( 'should not be available when paused or pending', function (done) {
+    flow = new Flow({
+      pause: function () {
+        this.wait();
+      },
+      pend: function () {
+        var pender = new Flow(function () {
+          this.wait(done, 0);
+        });
+        pender.go(1);
+      }
+    });
+    flow.go('//pause');
+    flow.status().paused.should.be.ok;
+    flow.should.not.haveOwnProperty('vars');
+    flow.go('//pend');
+    flow.status().paused.should.not.be.ok;
+    flow.status().pending.should.be.ok;
+    flow.should.not.haveOwnProperty('vars');
+  });
+
+  it( 'should preserve pre-existing `.vars`', function () {
+    var
+      externalValue = {},
+      spy = sinon.spy(function () {
+        this.vars.should.be.empty;
+        this.vars.should.be.an('object');
+        this.vars.should.not.equal(externalValue);
+      })
+    ;
+    flow = new Flow(spy);
+    flow.vars = externalValue;
+    flow.go(1);
+    spy.should.have.been.calledOnce;
+    flow.vars.should.equal(externalValue);
   });
 
 });
