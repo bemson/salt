f84f8bad454c227013658276d49eead9ace587d5
Bemi Faison <bemson@gmail.com>
2013-05-14 12:51:13 -0700

    Clarified use of `_capture` with `true` and `false`

--- test/api.tag.capture.js
+++ test/api.tag.capture.js
@@ -20,22 +20,35 @@ describe( '_capture tag', function () {
     spy.should.have.been.calledOnce;
   });
 
-  it( 'should allow capturing all or no sub-instances', function () {
+  it( 'should capture all sub-instances when `true`', function () {
     var spy = sinon.spy();
     flow = new Flow({
       _sequence: 1,
       all: {
         _capture: true,
         _in: function () {
+          this.subs().should.have.lengthOf(0);
           new Flow();
           new Flow();
+          this.subs().should.have.lengthOf(2);
         },
         _on: function () {
           this.subs().should.have.lengthOf(2);
           this.subs('remove', true);
           this.subs().should.have.lengthOf(0);
+          spy();
         }
-      },
+      }
+    });
+    flow.go(1);
+    flow.subs().should.equal(0);
+    spy.should.have.been.calledOnce;
+  });
+
+  it( 'should capturing no sub-instances when `false`', function () {
+    var spy = sinon.spy();
+    flow = new Flow({
+      _sequence: 1,
       none: {
         _capture: false,
         _in: function () {
