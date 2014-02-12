38290944ba63af8bc8c69c8c6228aeed4877f072
Bemi Faison <bemson@gmail.com>
2014-02-11 23:51:32 -0800

    Revised `_sequence` test

--- test/api.tag.sequence.js
+++ test/api.tag.sequence.js
@@ -2,7 +2,7 @@ describe( '_sequence tag', function () {
 
   var salt;
 
-  it( 'should navigate descendants of a state', function () {
+  it( 'should navigate to all descendent states', function () {
     var
       seqSpy = sinon.spy(),
       aSpy = sinon.spy(),
@@ -23,41 +23,41 @@ describe( '_sequence tag', function () {
     salt.state.path.should.equal('//seq/b/');
   });
 
-  it( 'should not work targeting a descendant of the tagged state', function () {
-    var
-      aSpy = sinon.spy(),
-      bSpy = sinon.spy()
-    ''
-    salt = new Salt({
-      seq: {
-        a: aSpy,
-        b: bSpy
-      }
-    });
-    salt.go('//seq/a/');
-    aSpy.should.have.been.calledOnce;
-    bSpy.should.not.have.been.called;
-    salt.state.path.should.equal('//seq/a/');
-  });
+  it( 'should exclude tagged states from a sequence, when paired with a falsy value', function () {
+    var spy = sinon.spy();
 
-  it( 'should add descendants as waypoints', function () {
-    var aSpy = sinon.spy();
     salt = new Salt({
-      seq: {
-        _sequence: 1,
-        a: {
-          _in: function () {
-            this.status().targets.should.eql(['//seq/a/','//end/']);
-            aSpy();
-          }
-        }
+      _sequence: 1,
+      skip: {
+        _sequence: 0,
+        _over: spy
       },
       end: {}
     });
-    salt.go('//seq/', '//end/');
-    aSpy.should.have.been.calledOnce;
+    salt.go(1);
+    spy.should.have.been.calledOnce;
     salt.state.path.should.equal('//end/');
   });
 
+  it( 'should add waypoints after the "in" phase', function () {
+    var spy = sinon.spy();
+
+    salt = new Salt({
+      _sequence: 1,
+      _in: function () {
+        this.status().targets
+          .should.have.lengthOf(1)
+          .and.eql(['//']);
+      },
+      _on: function () {
+        this.status().targets.should.eql(['//a/','//b/']);
+        spy();
+      },
+      a: {},
+      b: {}
+    });
+    salt.go(1);
+    spy.should.have.been.calledOnce;
+  });
 
 });
